ba7352c3fa2f396f4b34ec02fa8210989474c4e8
peter.toth10 <peter.toth10@MCC024396>
2017-01-18 17:06:23 +0100

    Add project files.

---
+++ DioneST/OracleEngine/OracleQueries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DioneST.OracleEngine
+{
+    public class OracleQueries
+    {
+        string oraConnString = "Data Source=(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = XE)));User Id=MAP;Password=dorisz";
+
+
+    }
+}
---
+++ DioneST/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DioneST.Startup))]
+namespace DioneST
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
